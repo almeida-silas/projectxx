cff08ec4395149875f773dbcafe1d04051b91d93
Silas Almeida <silasadssn@gmail.com>
2019-10-27 23:53:22 -0300

    Layout para os jogos

--- layoutfase.c
+++ layoutfase.c
@@ -1,14 +1,12 @@
+char layout(int fase) {
 
-char layout(int fase){
-
-         // Saída de dados; -- Menu;
+    // Saída de dados -- Menu
 	printf("\t\t\t==================================================\n");
 	printf("\t\t\t=                                                =\n");
 	printf("\t\t\t=              JOGO DA MATEMÁTICA                =\n");
-	printf("\t\t\t=                    Fase %i                     =\n", fase); // Mostrando número da fase com o argumento;
+	printf("\t\t\t=                    Fase %i                     =\n", fase); // Mostrando número da fase com o argumento
 	printf("\t\t\t==================================================\n");
-	printf("\t\t\t=                                                =\n");
+    printf("\t\t\t=                                                =\n");
 	printf("\t\t\t==================================================\t\n");
-
 }
 
