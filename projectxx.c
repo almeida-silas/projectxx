0d57fc41544cd4aa26976b6366018b8d778d23ff
Silas Almeida <silasadssn@gmail.com>
2019-10-27 23:52:10 -0300

    Parte inical

--- projectxx.c
+++ projectxx.c
@@ -1,13 +1,13 @@
-// InclusÃ£o de bibliotecas;
+// Inclusão de bibliotecas
 #include <stdio.h>		
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
 #include <math.h>
 #include <time.h>
-//#include <windows.h>
+#include <windows.h> // biblioteca windows
 
-// Incluindo outras partes do projeto;
+// Incluindo outras partes do projeto
 #include "jogov.c"
 #include "forca.c"
 #include "menumat.c"
@@ -19,106 +19,51 @@
 #include "fase3.c"
 #include "dicas.c"
 
-    typedef struct {
-        char nome[30];
-        
-    }registro;
-
-    // VariÃ¡veis global;
-	int gInicio = 0;
-    registro cadastro;
-
 int main() {
 
+	// Escolhendo idioma
     setlocale(LC_ALL, "Portuguese");
-    // Escolhendo idioma;
 
-    // DeclaraÃ§Ã£o das VariÃ¡veis;
+    // Declaração das Variáveis
     int escolha;
-    void menu_jogos(void); void cadast(void); void sobre(void);
+	void menu_jogos(void); void cadast(void); void sobre(void);
 
-    if(gInicio == 1) {
+	system("cls"); // limpa console
+    system("color 02"); // Muda as cores
+    
+	// Saída de dados -- Menu Principal
+    printf("\t\t\t==================================================\n");
+    printf("\t\t\t=                                                =\n");
+    printf("\t\t\t=                      INICIO                    =\n");
+    printf("\t\t\t=                                                =\n");
+	printf("\t\t\t==================================================\n");
+	printf("\t\t\t=     || Bem-vindo    ||                         =\n");
+	printf("\t\t\t==================================================\n");
+	printf("\t\t\t=        Jogos [1] | Dicas [2] | Sobre [3]       =\n"); // Mostrando opções
+	printf("\t\t\t==================================================\t");
+	
+	// Entrada de dados - Escolhendo opções
+	scanf("%i", &escolha);
 
-        system("cls" || "clear");
-//        system("color 02"); // Muda as cores;
-
-        // SaÃ­da de dados -- Menu Principal;
-        printf("\t\t\t==================================================\n");
-        printf("\t\t\t=                                                =\n");
-        printf("\t\t\t=                      INICIO                    =\n");
-        printf("\t\t\t=                                                =\n");
-        printf("\t\t\t==================================================\n");
-        printf("\t\t\t=     || Bem-vindo    ||                         =\n");
-        // Mostrando opÃ§Ãµes;
-        printf("\t\t\t==================================================\n");
-        printf("\t\t\t=        Jogos [1] | Dicas [2] | Sobre [3]       =\n"); 
-        printf("\t\t\t==================================================\t");
-        // Entrada de dados - Escolhendo opÃ§Ãµes;
-        scanf("%i", &escolha);
-
-        if(escolha < 1 || escolha > 3) {
-            // SaÃ­da de dados - Mostra ao usuÃ¡rio erro;
-            printf("\t\t\t  OpÃ§Ã£o invÃ¡lida!!\n"); 
-            printf("\t\t\t  ");
-			system("pause");
-			main();
-        }
-        // OpÃ§Ãµes do menu;
-        switch(escolha) {
-            case 1:
-                menu_jogos(); // Entra no menu de jogos;
-                break;
-
-            case 2:
-                dicas();
-                break;
-                
-            case 3:
-                sobre(); // Entra em sobre;
-                break;
-        }
-    } else {
-		cadast(); // Vai para o cadastro;
-    }
-	return 0;
-}
-
-void cadast(void) { // Cadastro;
-
-		// DeclaraÃ§Ã£o das variÃ¡veis;
-		int tamanho, loop = 0;
-
-	// Registro do jogador; Repete SE errado;
-
-    while(loop != 1) {
-
-		printf("\t\t\t\t                 | eXGames | ");
-		printf("\n\t\t\t\t--| INFORME ALGUNS DADOS PARA PROSSEGUIR |--\n");
-
-    	// Entrada de dados;
-    	printf("Nome: ");
-    	scanf("%s", cadastro.nome);
-
-		    tamanho = strlen(cadastro.nome); // Contando letras;
-
-    	if(tamanho > 4) { // CondiÃ§Ã£o para cadastro;
-
-//    	    system("cls" || "clear");
-
-            // Encerramento dos loops;
-        	loop = 1;
-			gInicio = 1;
-
-			printf("Cadastro OK!!\n"); // SaÃ­da de dados
-			// system("pause"); 
-			main(); // PÃ¡gina principal;
-
-	    } else {
-
-			system("cls" || "clear");
-			// SaÃ­da de dados - Mensagem de erro;
-            printf("\aPor favor, informe um nome real!\n"); 
-
-		}
+	if(escolha < 1 || escolha > 3) {
+	
+		printf("\t\t\t  Opção inválida!!\n"); // Saída de dados - Mostra ao usuário erro
+		printf("\t\t\t  ");
+	
+		system("pause"); 
+		main();
 	}
+
+	switch(escolha) {// Opções do menu
+        case 1:
+			menu_jogos(); // Entra no menu de jogos
+            break;
+		case 2:
+			dicas(); // entra no menu de dicas
+			break;
+    	case 3:
+			sobre(); // Entra em sobre
+            break;
+        }
+	return 0;
 }
