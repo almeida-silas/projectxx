623ce47867a3eba8cf37540fe0c0d80629e27102
Silas Almeida <silasadssn@gmail.com>
2019-10-27 23:50:42 -0300

    Fase 1 - jogo da matématica

--- fase1.c
+++ fase1.c
@@ -1,49 +1,48 @@
+void fase1 (void){ // Fase 1 do jogom
 
-void fase1 (void){ // Fase 1 do jogom;
-
-    // Gerando números randomicos;
+    // Gerando números randomicos
     srand(time(NULL));
     int num = rand() % 100;
     int num2 = rand() % 100;
     int numalea = rand() % 2;
 
-    // Declaração das variáveis;
+    // Declaração das variáveis
     int resposta = 0, resposta_c = 0;
     char voltar; char layout(int fase);
 
-//    system("cls");
+    system("cls");
 
-    layout(1); // Mostrando Layout|Enfeite;
+    layout(1); // Mostrando Layout|Enfeite
 
-    if(numalea == 1){ // Aleatorizando os sinais adição e subtração;
-            printf("\t\t\t  Adição e Subtração!! \n"); // Exibe pontuação;
+    if(numalea == 1) { // Aleatorizando os sinais adição e subtração
+            printf("\t\t\t  Adição e Subtração!! \n"); // Exibe pontuação
             printf("\t\t\t  %i + %i = ", num, num2);
             resposta_c = num + num2;
     }else{
-        printf("\t\t\t  Adição e Subtração!!\n"); // Exibe pontuação;
+        printf("\t\t\t  Adição e Subtração!!\n"); // Exibe pontuação
         printf("\t\t\t  %i - %i = ", num, num2);
         resposta_c = num - num2;
     }
         scanf("%i", &resposta);
 
-    if(resposta == resposta_c){
+    if(resposta == resposta_c) {
 
-        printf("\t\t\t  Correto!!!\n\n"); // Saída de dados - Mostra acerto;
+        printf("\t\t\t  Correto!!!\n\n"); // Saída de dados - Mostra acerto
 
         printf("\t\t\t  Voltar [B] - Continuar [C] ");
         scanf("%s", &voltar);
 
-            if(voltar == 'B' || voltar == 'b'){
+            if(voltar == 'B' || voltar == 'b') {
                 jogom();
-            }else{
+            } else {
                 printf("\n\t\t\t\t ");
-                // system("pause"); 
+				system("pause"); 
                 fase1(); // Repete fase;
             }
-    }else{
+    } else {
     	
-        printf("\t\t\t  Errou!!\n\n"); // Saída de dados - Mostra erro;
-        printf("\t\t\t  A resposta correta é: %i \n", resposta_c); // Mostrando o resultado correto;
+        printf("\t\t\t  Errou!!\n\n"); // Saída de dados - Mostra erro
+        printf("\t\t\t  A resposta correta é: %i \n", resposta_c); // Mostrando o resultado correto
 
         printf("\t\t\t  Voltar [B] - Continuar [C] ");
         scanf("%s", &voltar);
@@ -52,7 +51,7 @@ void fase1 (void){ // Fase 1 do jogom;
                 jogom();
             }else{
                 printf("\n\t\t\t");
-                // system("pause"); 
+                system("pause"); 
                 fase1(); // Repete fase;    
             }
     }
